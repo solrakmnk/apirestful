1805fbb5ed3927a25195242f6fba7107e3c8a4a4
Carlos A. Guevara <solrak.m@gmail.com>
2017-08-19 00:33:53 -0500

    Modelos

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const PRODUCTO_DISPONIBLE='disponible';
+    const PRODUCTO_NO_DISPONIBLE='no disponible';
+
+    protected $fillable=[
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+    public function estaDisponible(){
+        return $this->status==Prodcuct::PRODDUCTO_DISPONIBLE;
+    }
 }
