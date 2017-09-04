90c0dbc3e804aa6dc128b67468cb03ca02f078b7
Carlos A. Guevara <carlos.guevara@entrepids.com>
2017-09-04 16:48:24 -0500

    Operaciones basicas usuario

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -29,7 +29,23 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules=[
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:6|confirmed'
+        ];
+
+        $this->validate($request,$rules);
+
+        $campos=$request->all();
+        $campos['password']=bcrypt($request->password);
+        $campos['verified']=User::USUARIO_NO_VERIFICADO;
+        $campos['verification_token']=User::generaVerificationToken();
+        $campos['admin']=User::USUARIO_REGULAR;
+
+        $usuario=User::create($campos);
+
+        return response()->json(['data'=>$usuario],200);
     }
 
     /**
@@ -55,7 +71,44 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user=User::findOrFail($id);
+
+        $rules=[
+        'email'=>'email|unique:users,email,'.$id,
+        'password'=>'min:6|confirmed',
+        'admin'=>'in:'.User::USUARIO_ADMINISTRADOR.",".User::USUARIO_REGULAR,
+    ];
+        $this->validate($request,$rules);
+
+        if($request->has('name')){
+            $user->name=$request->name;
+        }
+        if($request->has('email') && $user->email!=$request->email){
+            $user->verified=User::USUARIO_NO_VERIFICADO;
+            $user->verification_token=User::generaVerificationToken();
+            $user->email=$request->email;
+        }
+
+        if($request->has('password')){
+            $user->password=bcrypt($request->getPassword());
+        }
+
+        if($request->has('admin')){
+            if(!$user->esVerificado()){
+                return response()->json(['error'=>'Unicamente usuarios verificados pueden cambiar su valor como admin',
+                                         'code'=>409],409);
+            }
+            $user->admin=$request->admin;
+        }
+
+        if(!$user->isDirty()){
+            return response()->json(['error'=>'Se debe especificar al menos un valor diferente', 'code'=>422],422);
+        }
+
+        $user->save();
+
+        return response()->json(['data'=>$user],200);
+
     }
 
     /**
@@ -66,6 +119,9 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user=User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['data'=>$user],200);
     }
 }
