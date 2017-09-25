b9c6cd5101b4510ca06b86f54caa7c76062f4159
Carlos A. Guevara <carlos.guevara@entrepids.com>
2017-09-25 17:34:22 -0500

    Transformers

---
+++ app/Transformers/ProductTransformer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Product;
+use League\Fractal\TransformerAbstract;
+
+class ProductTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Product $product)
+     {
+         return [
+             'identificador' => (int)$product->id,
+             'titulo' => (string)$product->name,
+             'detalles' => (string)$product->description,
+             'disponibles' => (string)$product->quantity,
+             'estado' => (string)$product->status,
+             'imagen' => url("img/{$product->image}"),
+             'vendedor' => (int)$product->seller_id,
+             'fechaCreacion' => (string)$product->created_at,
+             'fechaActualizacion' => (string)$product->updated_at,
+             'fechaEliminacion' => isset($product->deleted_at) ? (string) $product->deleted_at : null,
+         ];
+     }
+}
---
+++ app/Transformers/SellerTransformer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Seller;
+use League\Fractal\TransformerAbstract;
+
+class SellerTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Seller $seller)
+     {
+         return [
+             'identificador' => (int)$seller->id,
+             'nombre' => (string)$seller->name,
+             'correo' => (string)$seller->email,
+             'esVerificado' => (int)$seller->verified,
+             'fechaCreacion' => (string)$seller->created_at,
+             'fechaActualizacion' => (string)$seller->updated_at,
+             'fechaEliminacion' => isset($seller->deleted_at) ? (string) $seller->deleted_at : null,
+         ];
+     }
+}
