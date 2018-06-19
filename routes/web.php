3d9659f457d303aac58ae4b629627fdf41afbce5
Carlos A. Guevara <carlos.guevara@pagofacil.net>
2018-06-19 15:14:20 -0600

    Personal access token component

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@
     $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
     $this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
+
+Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
 Route::get('/home', 'HomeController@index')->name('home');
 
     Route::get('/', function () {
