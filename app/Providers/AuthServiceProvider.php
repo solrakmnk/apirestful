086b01b43e2aa01ac076120271b68b374cce3d5a
Carlos A. Guevara <carlos.guevara@pagofacil.net>
2018-06-22 11:24:53 -0600

    UserPolicy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,9 @@ namespace App\Providers;
 use App\Buyer;
 use App\Policies\BuyerPolicy;
 use App\Policies\SellerPolicy;
+use App\Policies\UserPolicy;
 use App\Seller;
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -22,7 +24,8 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
        // 'App\Model' => 'App\Policies\ModelPolicy',
         Buyer::class=>BuyerPolicy::class,
-        Seller::class=>SellerPolicy::class
+        Seller::class=>SellerPolicy::class,
+        User::class=>UserPolicy::class
     ];
 
     /**
