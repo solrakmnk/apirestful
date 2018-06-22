3946b9264d00865390b8d46ff9c6ab0edea15ac7
Carlos A. Guevara <carlos.guevara@pagofacil.net>
2018-06-22 10:42:54 -0600

    SellerPolicy

---
+++ app/Policies/SellerPolicy.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Seller;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class SellerPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the seller.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Seller  $seller
+     * @return mixed
+     */
+    public function view(User $user, Seller $seller)
+    {
+        return $user->id===$seller->id;
+    }
+
+    /**
+     * Determine whether the user can sale.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Seller  $seller
+     * @return mixed
+     */
+    public function sale(User $user, User $seller)
+    {
+        return $user->id===$seller->id;
+    }
+
+    /**
+     * Determine whether the user can edit.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Seller  $seller
+     * @return mixed
+     */
+    public function editProduct(User $user, Seller $seller)
+    {
+        return $user->id===$seller->id;
+    }
+
+    /**
+     * Determine whether the user can delete.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Seller  $seller
+     * @return mixed
+     */
+    public function deleteProduct(User $user, Seller $seller)
+    {
+        return $user->id===$seller->id;
+    }
+}
